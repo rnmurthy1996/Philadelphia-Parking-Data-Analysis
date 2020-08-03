7aa10696221188767c6863720f7820fccc3e2135
jakeoeding <jaoeding@seas.upenn.edu>
2020-08-03 15:27:52 -0500

    initial project set up

---
+++ src/edu/upenn/cit594/datamanagement/JsonParkingViolationReader.java
@@ -0,0 +1,15 @@
+package edu.upenn.cit594.datamanagement;
+
+import java.util.List;
+
+import edu.upenn.cit594.data.ParkingViolation;
+
+public class JsonParkingViolationReader implements ParkingViolationReader {
+
+	@Override
+	public List<ParkingViolation> getAllParkingViolations() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
