41c2310f7dc5a2f475003613f54cb154e5bd3339
jakeoeding <jaoeding@seas.upenn.edu>
2020-08-06 17:48:33 -0500

    implemented logger functionality

--- src/edu/upenn/cit594/logging/Logger.java
+++ src/edu/upenn/cit594/logging/Logger.java
@@ -1,5 +1,28 @@
 package edu.upenn.cit594.logging;
 
-public class Logger {
+import java.io.FileWriter;
+import java.io.PrintWriter;
 
+public class Logger {
+	public static String logName;
+	private static Logger instance;
+	private PrintWriter out;
+
+	private Logger() {
+		try { out = new PrintWriter(new FileWriter(logName, true)); }
+		catch (Exception e) { }
+	}
+	
+	public static Logger getInstance() {
+		if (instance == null) {
+			instance = new Logger();
+		}
+		return instance;
+	}
+	
+	public void log(String text) {
+		String output = String.format("%d %s\n", System.currentTimeMillis(), text);
+		out.write(output);
+		out.flush();
+	}
 }
